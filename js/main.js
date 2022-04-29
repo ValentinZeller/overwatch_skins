a96e766999ab413cbd933a10c49d02fe40d15785
ValentinZeller <zeller.valentin@hotmail.com>
2022-04-29 19:26:37 +0200

    Fetch json

--- js/main.js
+++ js/main.js
@@ -2,68 +2,79 @@ let container = document.getElementById('container');
 
 let totalEpic = 0;
 let totalLegendary = 0;
+let heroes = 0;
 
-generateCategoryRow();
+async function fetchSkins() {
+  let test = await fetch("https://api.jsonbin.io/b/6269619738be296761f8fbb8/2");
+  let json = await test.json();
+  heroes = json;
+  generateCategoryRow();
+  generateContent();
+}
 
-for (let hero in heroes) {
-  let row = generateDiv('row',hero);
-  container.appendChild(row);
+fetchSkins();
 
-  /* First column : hero picture */
-  let rowHeader = generateDiv('rowHeader');
-  row.appendChild(rowHeader);
-  rowHeader.style.background = "url('./images/" + hero + "/character.png') no-repeat scroll 50% 0% / cover";
+function generateContent() {
+  for (let hero in heroes) {
+    let row = generateDiv('row',hero);
+    container.appendChild(row);
 
-  let skinLegendaryCount = 0;
-  let skinEpicCount = 0;
+    /* First column : hero picture */
+    let rowHeader = generateDiv('rowHeader');
+    row.appendChild(rowHeader);
+    rowHeader.style.background = "url('./images/" + hero + "/character.png') no-repeat scroll 50% 0% / cover";
 
-  if(hero === "total") {
-    /* Last row : count */
-    for(category in heroes.total) {
-      row.appendChild(generateRowCount(heroes.total[category].epic,heroes.total[category].legendary));
-      skinEpicCount += heroes.total[category].epic;
-      skinLegendaryCount += heroes.total[category].legendary;
-    }
-  } else {
-      for(let category in heroes[hero]) {
-        let cell = generateDiv('cell',category);
-        row.appendChild(cell);
+    let skinLegendaryCount = 0;
+    let skinEpicCount = 0;
 
-        for (var i = 0; i < heroes[hero][category].length; i++) {
-            let skin = heroes[hero][category][i];
-            let item = generateDiv('item',skin.name);
-            item.setAttribute('data-tooltip',skin.name);
-            if (skin.display) {
-              item.setAttribute('data-tooltip',skin.display);
-            }
-            /* Skins per cell : split the width/height of each */
-            if (heroes[hero][category].length == 1) { // 1
-              item.setAttribute('class','item');
-            } else if (heroes[hero][category].length <= 2) { // 2
-              item.setAttribute('class','item half');
-            } else if (heroes[hero][category].length <= 4) { // 3-4
-              item.setAttribute('class','item quarter');
-            } else if (heroes[hero][category].length <= 8) { // 5-8
-              item.setAttribute('class','item eigth');
-            } else if (heroes[hero][category].length <= 16) { // 9-16
-              item.setAttribute('class','item sixteenth');
-            }
-
-            /* Add epic border and count epic/legendary skins */
-            if (skin.rarity === 0) {
-              item.classList.add('epic');
-              skinEpicCount++;
-              heroes.total[category].epic++;
-            } else {
-              skinLegendaryCount++;
-              heroes.total[category].legendary++;
-            }
-            cell.appendChild(item);
-            item.style.background = "url('./images/" + hero + "/" + skin.name + ".png') no-repeat scroll 50% 0% / cover";
-        }
+    if(hero === "total") {
+      /* Last row : count */
+      for(category in heroes.total) {
+        row.appendChild(generateRowCount(heroes.total[category].epic,heroes.total[category].legendary));
+        skinEpicCount += heroes.total[category].epic;
+        skinLegendaryCount += heroes.total[category].legendary;
       }
+    } else {
+        for(let category in heroes[hero]) {
+          let cell = generateDiv('cell',category);
+          row.appendChild(cell);
+
+          for (var i = 0; i < heroes[hero][category].length; i++) {
+              let skin = heroes[hero][category][i];
+              let item = generateDiv('item',skin.name);
+              item.setAttribute('data-tooltip',skin.name);
+              if (skin.display) {
+                item.setAttribute('data-tooltip',skin.display);
+              }
+              /* Skins per cell : split the width/height of each */
+              if (heroes[hero][category].length == 1) { // 1
+                item.setAttribute('class','item');
+              } else if (heroes[hero][category].length <= 2) { // 2
+                item.setAttribute('class','item half');
+              } else if (heroes[hero][category].length <= 4) { // 3-4
+                item.setAttribute('class','item quarter');
+              } else if (heroes[hero][category].length <= 8) { // 5-8
+                item.setAttribute('class','item eigth');
+              } else if (heroes[hero][category].length <= 16) { // 9-16
+                item.setAttribute('class','item sixteenth');
+              }
+
+              /* Add epic border and count epic/legendary skins */
+              if (skin.rarity === 0) {
+                item.classList.add('epic');
+                skinEpicCount++;
+                heroes.total[category].epic++;
+              } else {
+                skinLegendaryCount++;
+                heroes.total[category].legendary++;
+              }
+              cell.appendChild(item);
+              item.style.background = "url('./images/" + hero + "/" + skin.name + ".png') no-repeat scroll 50% 0% / cover";
+          }
+        }
+    }
+    row.appendChild(generateRowCount(skinEpicCount,skinLegendaryCount));
   }
-  row.appendChild(generateRowCount(skinEpicCount,skinLegendaryCount));
 }
 
 function generateCategoryRow() {
