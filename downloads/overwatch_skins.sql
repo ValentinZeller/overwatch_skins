dff8b7a701a1c2ae9fabe947f6f9d24ba27cdd28
Valentin <zeller.valentin@hotmail.com>
2026-03-30 23:31:47 +0200

    Change colors

--- downloads/overwatch_skins.sql
+++ downloads/overwatch_skins.sql
@@ -16,6 +16,16 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Current Database: `overwatch_skins`
+--
+
+/*!40000 DROP DATABASE IF EXISTS `overwatch_skins`*/;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `overwatch_skins` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `overwatch_skins`;
+
+--
 -- Table structure for table `category`
 --
 
@@ -205,4 +215,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-03-27 14:20:12
+-- Dump completed on 2026-03-27 17:18:10
